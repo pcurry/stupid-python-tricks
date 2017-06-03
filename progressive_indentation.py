110ccc1ce2a958980e58f7698aaf3e72b6c40e1b
Paul Curry <pcurry@gmail.com>
2017-06-03 18:21:07 -0400

    This works and it bothers me that it does

---
+++ progressive_indentation.py
@@ -0,0 +1,15 @@
+"""
+Demonstration of annoying indentation
+"""
+
+def foo(x):
+    print('foo of {}'.format(x))
+
+def bar(x):
+      print('bar of {}'.format(x))
+
+def baz(x):
+        print('baz of {}'.format(x))
+
+def baf(x):
+          print('baf of {}'.format(x))
