f948a55cbcdab6bc6a4ec2a14c36b9994ae9c9d8
Paul Curry <pcurry@gmail.com>
2017-06-05 12:44:09 -0400

    Added a comment about what annoyinf fact I was demonstrating

--- progressive_indentation.py
+++ progressive_indentation.py
@@ -1,15 +1,20 @@
 """
-Demonstration of annoying indentation
+Demonstrations of annoying indentation issues
 """
+
+# The following are all considered one level deep
 
 def foo(x):
     print('foo of {}'.format(x))
 
+
 def bar(x):
       print('bar of {}'.format(x))
 
+
 def baz(x):
         print('baz of {}'.format(x))
+
 
 def baf(x):
           print('baf of {}'.format(x))
